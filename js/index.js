aac79c5933f6f46dd8bf031881ac9bf9cc8f4a36
Mihail <ternovetchii.2002@mail.ru>
2023-03-25 10:39:53 +0200

    all work
    all work

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const navToggle = document.querySelector('.nav__toggle'),
-   navMenu = document.querySelector('.nav__menu');
-
-
-
-navToggle.addEventListener('click', () => {
-   navMenu.classList.toggle('show__menu');
-   navToggle.classList.toggle('active');
-});
